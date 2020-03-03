b37b4639b784d097f3ef8806eb108717feb64072
benjamingomez0 <benjamingomez0@gmail.com>
2020-03-03 15:53:25 -0800

    part 1 of 2.1 HW completed and formatted properly

--- HomeworkAssignment2_1.java
+++ HomeworkAssignment2_1.java
@@ -1,25 +1,34 @@
 
 import java.util.Scanner;
-
-public class HomeworkAssignment2_1{
-		public static void main(String[] args) {
-			
-			int pennies;
-			int change = 0;
+public class HomeworkAssignment2_1 {
+    public static void main(String args[]) {
+        Scanner reader = new Scanner(System.in); 
+        System.out.println("Please enter all of your pennies: ");
+        int numOfPennies = reader.nextInt(); 
+        Solution sol = new Solution();
+        System.out.println(sol.breakCash(numOfPennies));
+    }
+}
+class Solution {
+    /**
+    * FUNCTION SINGATURE:
+    * String breakCash(int numPennies)
+    * PURPOSE: the function takes in the total of pennies from the user as input, 
+    * computes its equivalent in dollars, quarters, dimes, nickels, and pennies, 
+    * and outputs the result in string to the console. 
+    * @param int numPennies
+    * @return String output in dollars, quarters, dimes, nickels, and pennies
+    */ 
+    public String breakCash(int numPennies) {
+        int change = 0;
 	        int dollars  = 0;
 	        int quarters = 0;
 	        int dimes    = 0;
 	        int nickels  = 0;
 	        
-	        Scanner in = new Scanner(System.in);
+	        dollars = numPennies/100;
 	        
-	        System.out.print("Please Enter the amount of Pennies: ");
-	        
-	        pennies = in.nextInt();
-	        
-	        dollars = pennies/100;
-	        
-	        change = pennies%100;
+	        change = numPennies%100;
 	        
 	        quarters = change/25;
 	        if(quarters!=0)
@@ -39,9 +48,8 @@ public class HomeworkAssignment2_1{
 	            change=change%5;
 	        }
 	        
-	        pennies = change; 
+	       return("dollars: "+dollars+" quarters: "+quarters+" dimes: "+dimes+" nickels: "+nickels+" pennies: "+change);
 	        
-	        System.out.println("dollars: "+dollars+" quarters: "+quarters+" dimes: "+dimes+" nickels: "+nickels+" pennies: "+pennies);
-	}
+    }
 }
 
