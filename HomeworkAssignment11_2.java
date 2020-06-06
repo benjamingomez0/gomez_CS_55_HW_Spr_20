693a3f8a5937d6a48e9666aabc35d2390eb926d8
benjamingomez0 <benjamingomez0@gmail.com>
2020-06-05 22:48:42 -0700

    start 11.2 problem

--- HomeworkAssignment11_2.java
+++ HomeworkAssignment11_2.java
@@ -1,6 +1,6 @@
-// /*     * Assignment #11.1
+// /*     * Assignment #11.2
 //         * DUE: 05/22/20
-//         * PURPOSE: Takes in a vector and prevents overflow
+//         * PURPOSE: takes in the vector representation of N and returns the result of ++N
 //         * Benjamin Gomez
 //         * Section 4107
 // */
@@ -10,18 +10,37 @@ import java.util.Vector;
 public class HomeworkAssignment11_2 {
    public static void main(String args[]) {
       Solution sol = new Solution();
-      Vector v = new Vector();
-      v.add(9);
+      Vector<Integer> v = new Vector<Integer>();
+      v.add(8);
       v.add(9); 
       System.out.println(sol.plusPlusN(v)); // [1,0,0]
    }
 }
-// YOUR DOCUMENTATION STYLING HERE
+/*       *
+          * FUNCTION SIGNATURE:  Vector plusPlusN(Vector v)
+          * PURPOSE: takes in the vector representation of N and returns the result of ++N
+          * @param Vector v
+          * @return Vector 
+          *
+ */
 class Solution {
-   //OR, YOUR DOCUMENTATION STYLING HERE
+  
    Vector plusPlusN(Vector v) {
-      System.out.println(v.size());
-      
+      v.add(0,1);
+      v.add(0,0);
+      boolean flag = false;
+      for(int i = v.size()-1;i>=0;i--)
+      {
+         if(v.get(i).equals(9))
+         {
+            v.set(i,0);
+            flag = true;
+            v.set((i-1),v.get(i-1)+1);
+            System.out.println(v.get(i));
+         }
+        System.out.println( v.get(i));
+      }
+      // System.out.println(v.get(i));
       return v;
 
 }
